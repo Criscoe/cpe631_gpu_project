fe29b2bff2bcedccb3ba349bbd883516531b0dc4
Criscoe <jmailbox02@gmail.com>
2025-04-20 00:35:52 -0500

    More samples and updates

--- spitballScripts/cudaSpitBall.py
+++ spitballScripts/cudaSpitBall.py
@@ -18,9 +18,14 @@ def benchmark_cpu(image, num_iterations):
 def benchmark_gpu(args, image, num_iterations):
     if not cv2.cuda.getCudaEnabledDeviceCount():
         raise RuntimeError("No CUDA-capable GPU detected or OpenCV not built with CUDA support.")
-
+    
+    # Set up gpu and upload image
+    startGpuSetup = time.time()
     gpu_img = cv2.cuda_GpuMat()
     gpu_img.upload(image)
+    endGpuSetup = time.time()
+    setupTime = endGpuSetup - startGpuSetup
+    print(f"Time taken for image upload: {setupTime:.4f}")
     upper = 50
     lower = 150
     if args.enable_img_result:
@@ -33,18 +38,26 @@ def benchmark_gpu(args, image, num_iterations):
         edged = canny.detect(gpu_img)
     end = time.time()
 
+    # Download resulting image from gpu
+    startGpuDownload = time.time()
+    result = edged.download()
+    endGpuDownload = time.time()
+    downloadTime = endGpuDownload - startGpuDownload
+    print(f"Time taken for image download: {downloadTime:.4f}")
+
     if args.enable_img_result:
-        cv2.imshow("Output", edged.download())
+        cv2.imshow("Output", result)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
-    return end - start
+    gpu_time = end - start
+    print(f"GPU Time: {gpu_time:.4f} seconds")
 
 def main():
     parser = argparse.ArgumentParser(description="Python benchmark for Canny edge detection using single/multi threaded CPU and GPU.")
     parser.add_argument("--enable-img-result", action="store_true", help="Enables printing of resulting image")
-    parser.add_argument("--num-iterations", type=int, default=1, help="Num iterations to execute on")
+    parser.add_argument("--num-iterations", type=int, default=1, help="Num iterations to execute calculations on")
     parser.add_argument("--num-cpu-threads", type=int, default=1, help="Number of cpu threads to use")
+    parser.add_argument("--benchmark-image", type=str, default='sample.jpg', help="Path and image name of benchmark image used")
     args = parser.parse_args()
     # Load an image (adjust size for a more demanding task)
     image = cv2.imread('sample.jpg')
@@ -64,8 +77,7 @@ def main():
 
     print("Starting GPU benchmark...")
     try:
-        gpu_time = benchmark_gpu(args, grayimg, num_iterations)
-        print(f"GPU Time: {gpu_time:.4f} seconds")
+        benchmark_gpu(args, grayimg, num_iterations)
     except RuntimeError as e:
         print(f"GPU Benchmark skipped: {e}")
 
