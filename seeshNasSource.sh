2499a0759cd2d9277a6951265a0fe57aa85c1bbd
Seesh <pietrzik.s@gmail.com>
2025-04-22 20:55:43 -0500

    delete echo

--- seeshNasSource.sh
+++ seeshNasSource.sh
@@ -4,5 +4,3 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 export CUDA_INJECTION64_PATH=${SCRIPT_DIR}/cupti_sandbox/libcupti_trace_injection.so
 export NVTX_INJECTION64_PATH=/seeshpool/calebs_hidey_hole/cuda/targets/x86_64-linux/lib/libcupti.sowh
-
-echo ${CUDA_INJECTION64_PATH}
